31b60fd7ca82d0940588aba85086176cd6cf5293
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2024-07-08 14:42:29 +0900

    minor fix

--- evil.el
+++ evil.el
@@ -19,6 +19,30 @@
 ;;
 ;;; Code:
 
+;;; macro
+
+(defmacro defadvice! (symbol arglist &optional docstring &rest body)
+  "Define an advice called SYMBOL and add it to PLACES.
+
+ARGLIST is as in `defun'. WHERE is a keyword as passed to `advice-add', and
+PLACE is the function to which to add the advice, like in `advice-add'.
+DOCSTRING and BODY are as in `defun'.
+
+\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
+  (declare (doc-string 3) (indent defun))
+  (unless (stringp docstring)
+    (push docstring body)
+    (setq docstring nil))
+  (let (where-alist)
+    (while (keywordp (car body))
+      (push `(cons ,(pop body) (ensure-list ,(pop body)))
+            where-alist))
+    `(progn
+       (defun ,symbol ,arglist ,docstring ,@body)
+       (dolist (targets (list ,@(nreverse where-alist)))
+         (dolist (target (cdr targets))
+           (advice-add target (car targets) #',symbol))))))
+
 ;;; evil
 
 (use-package evil
@@ -216,28 +240,6 @@
                   ('nil 'bar)
                   ("korean-hangul" 'hbar))))
       (setq-local evil-insert-state-cursor type)))
-
-  (defmacro defadvice! (symbol arglist &optional docstring &rest body)
-    "Define an advice called SYMBOL and add it to PLACES.
-
-ARGLIST is as in `defun'. WHERE is a keyword as passed to `advice-add', and
-PLACE is the function to which to add the advice, like in `advice-add'.
-DOCSTRING and BODY are as in `defun'.
-
-\(fn SYMBOL ARGLIST &optional DOCSTRING &rest [WHERE PLACES...] BODY\)"
-    (declare (doc-string 3) (indent defun))
-    (unless (stringp docstring)
-      (push docstring body)
-      (setq docstring nil))
-    (let (where-alist)
-      (while (keywordp (car body))
-        (push `(cons ,(pop body) (ensure-list ,(pop body)))
-              where-alist))
-      `(progn
-         (defun ,symbol ,arglist ,docstring ,@body)
-         (dolist (targets (list ,@(nreverse where-alist)))
-           (dolist (target (cdr targets))
-             (advice-add target (car targets) #',symbol))))))
 
   (progn
     ;; keep evil insert cursor status per input-method
